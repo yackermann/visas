c6a22dc655beea7a7421acd117f265e66f889f94
Niemand <yurii.akkermanov@gmail.com>
2014-08-03 04:45:49 +1200

    Merged countries.json with https://github.com/mledoze/countries. Added dist version of data. Builder upgraded.

--- builder.py
+++ builder.py
@@ -13,6 +13,12 @@ for file in os.listdir(bfolder):
 			data["features"].append(json.loads(f.read())["features"][0])
 
 
+with open("dist/world.geo.json", "w") as w:
+	w.write(json.dumps(data))
 
 with open("world.geo.json", "w") as w:
-	w.write(json.dumps(data))
+	w.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+
+with open("countries.json") as f:
+	with open("dist/countries.json","w") as w:
+		w.write(json.dumps(json.loads(f.read())))
