cd1cf7bfe02e7abcb0dc8be807765700d59f9529
Niemand <yurii.akkermanov@gmail.com>
2014-08-03 04:48:52 +1200

    Doch\!

--- builder.py
+++ builder.py
@@ -16,9 +16,6 @@ for file in os.listdir(bfolder):
 with open("dist/world.geo.json", "w") as w:
 	w.write(json.dumps(data))
 
-with open("world.geo.json", "w") as w:
-	w.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
-
 with open("countries.json") as f:
 	with open("dist/countries.json","w") as w:
 		w.write(json.dumps(json.loads(f.read())))
