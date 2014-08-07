c55ee38c005955936379b71f6aa8a244411f5729
Niemand <yurii.akkermanov@gmail.com>
2014-08-07 17:06:26 +1200

    Moved Visas to data/visa. Moved geo data ti data/geo.

--- beauty.py
+++ beauty.py
@@ -1,12 +1,34 @@
 import os
 import json
 
-bfolder = "data"
+bfolder = "data/geo"
+print("Starting validating " + bfolder + "/")
 for file in os.listdir(bfolder):
 	if file.endswith(".geo.json"):
-		data = ""
-		with open(bfolder + "/"+ file) as f:
-			data = json.loads(f.read())
+		try:
+			data = ""
+			with open(bfolder + "/"+ file) as f:
+				data = json.loads(f.read())
 
-		with open(bfolder + "/"+ file, "w") as w:
-			w.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+			with open(bfolder + "/"+ file, "w") as w:
+				w.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+		except:
+			print("Error while validating file: " + bfolder + "/" + file)
+print("Validation of " + bfolder + "/ completed.\n")
+
+
+vfolder = "data/visa"
+print("Starting validating " + vfolder + "/")
+for file in os.listdir(vfolder):
+	if file.endswith(".visa.json"):
+		try:
+			data = ""
+			with open(vfolder + "/"+ file) as f:
+				data = json.loads(f.read())
+
+			with open(vfolder + "/"+ file, "w") as w:
+				w.write(json.dumps(data, sort_keys=True, indent=4, separators=(',', ': ')))
+		except:
+			print("Error while validating file: " + bfolder + "/" + file)
+
+print("Validation of " + vfolder + "/ completed.")
--- build.py
+++ build.py
@@ -2,20 +2,32 @@ import os
 import json
 import datetime
 
-data = {
+geodata = {
 	"type" : "FeatureCollection",
 	"features": []
 }
-bfolder = "data"
+timestamp = datetime.datetime.utcnow().isoformat()
+
+bfolder = "data/geo"
 for file in os.listdir(bfolder):
 	if file.endswith(".geo.json"):
 		with open(bfolder + "/"+ file) as f:
-			data["features"].append(json.loads(f.read())["features"][0])
+			geodata["features"].append(json.loads(f.read())["features"][0])
 
-data["build_time"] = datetime.datetime.utcnow().isoformat()
-
+geodata["build_time"] = timestamp
 with open("dist/world.geo.json", "w") as w:
-	w.write(json.dumps(data))
+	w.write(json.dumps(geodata))
+
+
+visadata = []
+vfolder = "data/visa"
+for file in os.listdir(vfolder):
+	if file.endswith(".visa.json"):
+		with open(vfolder + "/"+ file) as f:
+			visadata.append(json.loads(f.read()))
+
+with open("dist/world.visa.json","w") as w:
+	w.write(json.dumps(visadata))
 
 with open("countries.json") as f:
 	with open("dist/countries.json","w") as w:
