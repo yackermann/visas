805876a6d93f5aa573755509d99b6b854beec0b3
Niemand <yurii.akkermanov@gmail.com>
2014-08-07 19:42:42 +1200

    Added TODO generator. P.S. Need to improve python code... It looks ugly...

--- build.py
+++ build.py
@@ -35,3 +35,50 @@ with open("dist/world.visa.json","w") as w:
 with open("data/info/countries.json") as f:
 	with open("dist/countries.json","w") as w:
 		w.write(json.dumps(json.loads(f.read())))
+
+
+countries_g = []
+countries_v = []
+countries = {}
+ 
+visas = []
+ 
+countries_temp = ""
+visas_temp = ""
+geo_temp = ""
+ 
+todo = ""
+with open("dist/world.visa.json") as f:
+	visas_temp = json.loads(f.read())
+ 
+with open("dist/world.geo.json") as f:
+	geo_temp = json.loads(f.read())
+ 
+with open("dist/countries.json") as f:
+	countries_temp = json.loads(f.read())
+ 
+for i in countries_temp:
+	countries[i["cca2"]] = i["name"]
+	countries_g.append(i['cca2'])
+	countries_v.append(i['cca2'])
+ 
+ 
+todo += "### Todo for visa data\n"
+for i in visas_temp:
+	if i['cca2'] in countries_v:
+		countries_v.remove(i['cca2'])
+ 
+for i in countries_v:
+	todo += "-" + i + " : " + countries[i] + "\n"
+ 
+todo += "\n### Todo for geo data\n"
+
+for i in geo_temp["features"]:
+	if i["id"] in countries_g:
+		countries_g.remove(i["id"])
+
+for i in countries_g:
+	todo += "-" + i + " : " + countries[i] + "\n"
+
+with open("TODO.md","w") as w:
+	w.write(todo)
